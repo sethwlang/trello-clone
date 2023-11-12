8a5356dc8ac1f21013f3e800e06f545b7a1661f9
Seth Lang <sethwlang@proton.me>
2023-11-12 16:10:33 -0500

    drag n Drop

--- src/default-board.js
+++ src/default-board.js
@@ -7,7 +7,7 @@ export default {
       name: 'todo',
       tasks: [
         {
-          description: '',
+          description: 'hello',
           name: 'first task',
           id: uuid(),
           userAssigned: null
