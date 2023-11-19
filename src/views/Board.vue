8e3f8d38ddf542f00fe79c55bf5c23b3976f39b1
Seth Lang <sethwlang@proton.me>
2023-11-19 16:29:13 -0500

    refactor

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,41 +1,13 @@
 <template>
   <div class="board">
     <div class="flex flex-row items-start">
-      <div class="column" 
+      <BoardColumn
       v-for="(column, $columnIndex) of board.columns" 
       :key="$columnIndex"
-      draggable="true"
-      @drop="moveTaskOrColumn($event, column.tasks), $columnIndex"
-      @dragover.prevent
-      @dragenter.prevent
-      @dragstart.self="pickupColumn($event, $columnIndex)">
-          <div class="flex items-center mb-2 font-bold">
-            {{ column.name }}
-          </div>
-          <div class="list-reset">
-
-          </div>
-          <div class="task" 
-          v-for="(task, $taskIndex) of column.tasks"
-          :key="$taskIndex"
-          draggable="true"
-          @dragstart="pickupTask($event, $taskIndex, $columnIndex)"
-          @click="goToTask(task)"
-          @dragover.prevent
-          @dragenter.prevent
-          @drop.stop="moveTaskOrColumn($event, column.tasks,$columnIndex, $taskIndex)">
-            <span class="w-full flex-no-shrinkfont-bold">
-              {{ task.name }}
-            </span>
-            <p v-if="task.description" class="w-full flex-no-shrink" mt-1 text-sm>
-              {{ task.description }}
-            </p>
-          </div>
-          <input type="text"
-          class="block p-2 w-full bg-transparent"
-          placeholder="+ Enter new task" 
-          @keyup.enter="createTask($event, column.tasks)"/>
-      </div>
+      :column="column"
+      :columnIndex="$columnIndex"
+      :board="board"
+      />
       <div class="column flex">
       <input
         class="p-2 mr-2 flex-grow"
@@ -56,7 +28,9 @@
 </template>
 <script>
 import { mapState } from 'vuex'
+import BoardColumn from '../components/BoardColumn.vue';
 export default {
+  components:{BoardColumn},
   data(){
     return{
       newColumnName:'',
@@ -69,75 +43,20 @@ export default {
     }
   },
   methods:{
-    goToTask(task){
-     this.$router.push({name:'task', params:{id: task.id}})
-    },
     close(){
       this.$router.push({name: 'board'})
     },
-    createTask(e, tasks){
-      this.$store.commit('CREATE_TASK', {
-        tasks,
-        name: e.target.value
-      })
-      e.target.value='';
-    }, 
     createColumn () {
       this.$store.commit('CREATE_COLUMN', {
         name: this.newColumnName,
       })
       this.newColumnName = ''
     },
-    pickupTask(e, fromTaskIndex, fromColumnIndex){
-      e.dataTransfer.effectAllowed ='move';
-      e.dataTransfer.dropEffect ='move';
-
-      e.dataTransfer.setData('from-task-index', fromTaskIndex);
-      e.dataTransfer.setData('from-column-index', fromColumnIndex);
-      e.dataTransfer.setData('type', 'task')
-    },
-    pickupColumn (e, columnIndex) {
-      e.dataTransfer.effectAllowed = 'move'
-      e.dataTransfer.dropEffect = 'move'
-    
-      e.dataTransfer.setData('from-column-index', columnIndex)
-      e.dataTransfer.setData('type', 'column')
-    },
-    moveTaskOrColumn (e, toTasks, toColumnIndex, toTaskIndex) {
-      const type = e.dataTransfer.getData('type')
-      if (type === 'task') {
-        this.moveTask(e, toTasks, toTaskIndex !== undefined ? toTaskIndex : toTasks.length)
-      } else {
-        this.moveColumn(e, toColumnIndex)
-      }
-    },
-    moveTask(e, toTasks, toTaskIndex){
-      const fromColumnIndex = e.dataTransfer.getData('from-column-index')
-      const fromTasks = this.board.columns[fromColumnIndex].tasks
-      const fromTaskIndex = e.dataTransfer.getData('from-task-index')
-      
-      this.$store.commit('MOVE_TASK',{
-        fromTasks,
-        fromTaskIndex,
-        toTasks,
-        toTaskIndex
-
-      })
-    },
-    moveColumn (e, toColumnIndex) {
-      const fromColumnIndex = e.dataTransfer.getData('from-column-index')
-      this.$store.commit('MOVE_COLUMN', {
-        fromColumnIndex,
-        toColumnIndex
-      })
-    } 
   }
 }
 </script>
 <style lang="css">
-.task {
-  @apply flex items-center flex-wrap shadow mb-2 py-2 px-2 bg-white text-grey-darkest no-underline;
-}
+
 
 .column {
   @apply bg-grey-light p-2 mr-4 text-left shadow;
